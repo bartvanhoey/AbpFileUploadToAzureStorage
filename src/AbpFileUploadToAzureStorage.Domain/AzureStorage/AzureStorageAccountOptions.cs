109047735d66e2f3499d8b783210320cdd973d67
bartvanhoey <bartvanhoey@hotmail.com>
2021-02-09 21:44:57 +0100

    Update

---
+++ src/AbpFileUploadToAzureStorage.Domain/AzureStorage/AzureStorageAccountOptions.cs
@@ -0,0 +1,8 @@
+namespace AbpFileUploadToAzureStorage
+{
+  public class AzureStorageAccountOptions
+  {
+    public string ConnectionString { get; set; }
+    public string AccountUrl { get; set; }
+  }
+}
--- src/AbpFileUploadToAzureStorage.Domain/AzureStorageAccountOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace AbpFileUploadToAzureStorage
-{
- public class AzureStorageAccountOptions
-    {
-        public string ConnectionString { get; set; }
-        public string AccountUrl { get; set; }
-    }
-}
